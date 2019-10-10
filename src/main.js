4ad4701875ede0d4f0f07c76dea586f1a6bbcf7a
Rahmat Ade Saputra <gen781@googlemail.com>
2019-10-10 21:52:08 +0700

    tambah vuex, router & firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import {store} from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: "AIzaSyCZ6eb8oM_FcGXJszIxAeiMrISYQAe50jc",
+  authDomain: "vuefire-88345.firebaseapp.com",
+  databaseURL: "https://vuefire-88345.firebaseio.com",
+  projectId: "vuefire-88345",
+  storageBucket: "vuefire-88345.appspot.com",
+  messagingSenderId: "77144130978",
+  appId: "1:77144130978:web:2f7b4755e2a825a58a9efb"
+}
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="title">What do I need to do today?</div>
+    <input v-model="myTodo" /><button @click="addToDo">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      myTodo: ''
+    }
+  },
+  methods: {
+    addToDo: function () {
+      console.log('myTodo: ' + this.myTodo)
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing:border-box;
+}
+
+body, html, #app {
+  background:#f0f3f5;
+}
+
+.home {
+  width:300px;
+  margin:auto;
+}
+
+#vue-logo {
+  width:100px;
+}
+
+input, button {
+  border:0;
+  width:100%;
+  margin:0 0 10px;
+  padding:7px;
+}
+
+input {
+  font-size:12px;
+}
+
+button {
+  background:#0fb87f;
+  border:0;
+  text-transform:uppercase;
+  color:#fff;
+  font-weight:700;
+  cursor:pointer;
+}
+
+.title {
+  font-size:14px;
+  font-weight:700;
+  padding:0 0 10px 0;
+  margin:0 0 10px 0;
+  border-bottom:1px solid #666;
+}
+
+#errors {
+  background:#a52222;
+  color:#fff;
+  padding:5px;
+}
+</style>
